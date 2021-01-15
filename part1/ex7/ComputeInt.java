034b3e64a0de054dff5dd1334778516131495be9
KonanVarvar3 <73252817+KonanVarvar3@users.noreply.github.com>
2021-01-15 14:19:39 +0300

    Add files via upload

--- part1/ex7/ComputeInt.java
+++ part1/ex7/ComputeInt.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class ComputeInt {
 
     //Ex7
-    public void actionsWithNumbers() throws OutOfRangeException {
+    public void actionsWithNumbers(long number1, long number2) throws OutOfRangeException {
 
         Scanner scanner = new Scanner(System.in);
 
-        int num1 = (int) scanner.nextLong((int) 4294967295L);
-        int num2 = (int) scanner.nextLong((int) 4294967295L);
+        int num1 = Math.toIntExact(Integer.toUnsignedLong(Integer.parseInt(scanner.nextLine())));
+        int num2 = Math.toIntExact(Integer.toUnsignedLong(Integer.parseInt(scanner.nextLine())));
 
         if (Integer.toUnsignedLong(num1) < 0 || Integer.toUnsignedLong(num1) > 4294967295L ||
                 Integer.toUnsignedLong(num2) < 0 || Integer.toUnsignedLong(num2) > 4294967295L) {
